dba2dc85d1154b6b908f2dbbc4b74c615d409592
DimaSimakov <dimarodeo@gmail.com>
2023-09-12 01:08:50 +0700

    Created logout for users

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     ]
